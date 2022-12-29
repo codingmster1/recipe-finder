bdb812401ae58b24ddd02742a4038c1725b6dfad
Jeff <doylejeff98@gmail.com>
2022-12-29 15:16:42 -0500

    urls added in javascript and hover affect changed

--- recipe.js
+++ recipe.js
@@ -2,3 +2,8 @@ const appId = "d94b65c9";
 const appKey = "caa85b8af6dbce63b203c7eb3696d6f1";
 const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=d94b65c9&app_key=caa85b8af6dbce63b203c7eb3696d6f1&ingr=5-8&diet=low-carb&health=low-sugar&cuisineType=American&mealType=Dinner&dishType=Main%20course&calories=100-300';
 
+function loadRecipes(type = "paneer") {
+
+    const url = baseUrl + `&q=${type}`;
+    fetch(url)
+}
