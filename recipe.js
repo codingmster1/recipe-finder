aa666e09bc82db6e79e0d1e2f8c5fa0d0dc33d1e
Jeff <doylejeff98@gmail.com>
2022-12-30 14:28:44 -0500

    insert adjacent typo fixed

--- recipe.js
+++ recipe.js
@@ -1,6 +1,6 @@
 const appId = "d94b65c9";
 const appKey = "caa85b8af6dbce63b203c7eb3696d6f1";
-const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=d94b65c9&app_key=caa85b8af6dbce63b203c7eb3696d6f1&ingr=5-8&diet=low-carb&health=low-sugar&cuisineType=American&mealType=Dinner&dishType=Main%20course&calories=100-300';
+const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${appId}&app_key=${appKey}`;
 const recipeContainer = document.querySelector("#recipe-container");
 function loadRecipes(type = "paneer") {
 
@@ -34,7 +34,7 @@ const renderRecipes = (recipeList=[]) => {
         </div>
     </div>`;
 
-    recipeContainer.insertAdAdjacentHTML("beforeend", htmlStr)
+    recipeContainer.insertAdjacentHTML("beforeend", htmlStr)
 
     });
 };
