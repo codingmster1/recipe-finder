2343b88e6bf3089d2c7605d472ddb62465c68859
Jeff <doylejeff98@gmail.com>
2022-12-30 14:05:55 -0500

    recipe html function edited for js and const added

--- recipe.js
+++ recipe.js
@@ -1,7 +1,7 @@
 const appId = "d94b65c9";
 const appKey = "caa85b8af6dbce63b203c7eb3696d6f1";
 const baseUrl = 'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=d94b65c9&app_key=caa85b8af6dbce63b203c7eb3696d6f1&ingr=5-8&diet=low-carb&health=low-sugar&cuisineType=American&mealType=Dinner&dishType=Main%20course&calories=100-300';
-
+const recipeContainer = document.querySelector("#recipe-container");
 function loadRecipes(type = "paneer") {
 
     const url = baseUrl + `&q=${type}`;
@@ -19,9 +19,9 @@ const renderRecipes = (recipeList=[]) => {
         const {label: recipeTitle, ingredientLines, image:recipeImage,
         } = recipeObj.recipe;
         const htmlStr= ` <div class="recipe">
-        <div class="recipe-title">Stuffed Chicken Breast</div>
+        <div class="recipe-title">${recipeTitle}</div>
         <div class="recipe-pic">
-            <img src="stuffedchicken3.jpg" alt="Recipe">
+            <img src="${recipeImage}" />
         </div>
         <div class="recipe-text">
             <ul>
