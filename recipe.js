8c27c204bd4d4b8f6e47e4634cc74fc4c017e0fe
Jeff <doylejeff98@gmail.com>
2023-01-03 01:56:07 -0500

    fixed txtsearch to render new recipes inputval

--- recipe.js
+++ recipe.js
@@ -1,15 +1,16 @@
 const appId = "d94b65c9";
 const appKey = "caa85b8af6dbce63b203c7eb3696d6f1";
-//const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${appId}&app_key=${appKey}`;
-const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=bread&app_id=${appId}&app_key=${appKey}&diet=low-carb`;
-//const baseUrl = ``;
+
+const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${appId}&app_key=${appKey}`;
+
+
 const recipeContainer = document.querySelector("#recipe-container");
 const txtSearch=document.querySelector("#txtSearch");
 
 txtSearch.addEventListener("keyup", (e)=> {
     const inputVal = txtSearch.value;
     if(e.keyCode === 13) {
-        loadRecipes()
+        loadRecipes(inputVal);
     }
 })
 
